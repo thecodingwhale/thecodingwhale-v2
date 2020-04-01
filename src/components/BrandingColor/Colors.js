67347379448d4ccd6bae926347727defc8759103
dev-12868979-2a <dev-12868979-2a@inscoder.com>
2020-04-01 19:35:59 +0800

    feat: add branding colors

---
+++ src/components/BrandingColor/Colors.js
@@ -0,0 +1,15 @@
+const lightGray = '#F7F7F7'
+const darkGray = '#3C4243'
+const primaryBrand = '#00F5FF'
+const secondaryBrand = '#076265'
+const primaryAccent = '#FE72FF'
+const secondaryAccent = '#FFFD4A'
+
+export {
+  lightGray,
+  darkGray,
+  primaryBrand,
+  secondaryBrand,
+  primaryAccent,
+  secondaryAccent,
+}

7d47ba64d8d8e2be27d3bfc246ce6779a097fca2
dev-12868979-2a <dev-12868979-2a@inscoder.com>
2020-04-01 22:07:43 +0800

    feat: setup and add fonts

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
-
     // Make whatever fine-grained changes you need
     config.module.rules.push({
       test: /\.scss$/,
@@ -15,6 +14,18 @@ module.exports = {
       include: path.resolve(__dirname, '../'),
     })
 
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
     // Return the altered config
     return config
   },
